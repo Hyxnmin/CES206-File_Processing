dd7677348766afbc5b6a64603d819e2a319ec124
Hyxnmin <phmi4199@gmail.com>
2023-11-29 16:44:28 +0900

    restore winner_tree.cpp

--- Fp_assignment1/winner_tree.cpp
+++ Fp_assignment1/winner_tree.cpp
@@ -1,114 +1,102 @@
 #include<iostream>
+#include<string>
 #include<fstream>
-#include<vector>
 
 using namespace std;
 
-#define BUCKET_SIZE 16
+#define MIN(x,y) (y>=x)?(x):(y) 
 
-typedef struct Bucket {
-    long long value;
-    long long key;
-}Bucket;
+long long file_data_count(string str) {
+    long long cnt = 0, data_check;
+    ifstream fin;
+    fin.open(str);
 
-void insert_value(vector<Bucket>& bucket, const long long key) {
-
-    const long long hash_key = key % BUCKET_SIZE;
-    long long  save_key = hash_key;
-
-    if (bucket[hash_key].value == -1) {
-        bucket[hash_key].value = key;
-    }
-    // check same value in bucket
-    else if (bucket[hash_key].value == key) {
-        return;
-    }
-    else {
-        for (long long i = 16; i < 32; ++i) {
-            // check same value in overflow bucket
-            if (bucket[i].value == key) {
-                return;
-            }
-            // save insert value's address for connect previous value
-            if (bucket[i].value % BUCKET_SIZE == hash_key && bucket[i].key == -1) {
-                save_key = i;
-            }
-        }
-        for (int i = 16; i < 32; ++i) {
-            if (bucket[i].value == -1) {
-                bucket[i].value = key;
-                bucket[save_key].key = i;
-                return;
-            }
-        }
-    }
-}
-
-void delete_value(vector<Bucket>& bucket, const long long key) {
-
-    const long long hash_key = key % BUCKET_SIZE;
-    long long cur = hash_key, next = bucket[hash_key].key;
-
-    // find delete value
-    while (bucket[cur].value != key && next != -1) {
-        cur = next, next = bucket[next].key;
-    }
-
-    // connect remove value's prev value and next value
-    if (next != -1) {
-        bucket[cur].value = bucket[next].value;
-        bucket[cur].key = bucket[next].key;
-        bucket[next].key = -1;
-        bucket[next].value = -1;
-    }
-
-    return;
-}
-
-// print sequentially bucket and overflow bucket 
-void print_value(vector<Bucket>& bucket) {
-
-    ofstream fout("result.txt");
-
-    for (int i = 0; i < 16; ++i) {
-        int cur = i;
-        fout << cur << ":";
-        // using key for print value
-        while (bucket[cur].key != -1) {
-            fout << bucket[cur].value << ",";
-            cur = bucket[cur].key;
-        }
-        // print remaining bucket[cur].value
-        fout << bucket[cur].value << ",\n";
-    }
-
-    fout.close();
-}
-
-int main() {
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-
-    // variables initialize
-    vector<Bucket> bucket(32, { -1,-1 });
-    long long read_key;
-    char cmd;
-
-    ifstream fin("hash_cmd.txt");
-
-    while (fin >> cmd >> read_key) {
-        if (cmd == 'i') {
-            insert_value(bucket, read_key);
-        }
-        else if (cmd == 'r') {
-            delete_value(bucket, read_key);
-        }
+    while (!fin.eof()) {
+        fin >> data_check;
+        cnt++;
     }
 
     fin.close();
 
-    print_value(bucket);
+    return cnt;
+
+}
+
+void read_file(string str, long long* read_input) {
+    int cnt = 0;
+    ifstream fin;
+    fin.open(str);
+
+    while (!fin.eof()) {
+        fin >> read_input[cnt++];
+    }
+    fin.close();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    long long** read_input = new long long* [8];
+    long long Binary_Tree[15] = { 0 }, cnt[8] = { 0 }, sp = 0, fp = 0, winner_path[4] = { 0 }, winner = 0, left = 0, right = 0;
+
+    for (long long i = 0; i < 8; ++i) {
+        read_input[i] = new long long[file_data_count("input_" + to_string(i) + ".txt")];
+        read_file("input_" + to_string(i) + ".txt", read_input[i]);
+    }
+
+    ofstream fout;
+    fout.open("merged_out.txt"); //merge_out.txt 파일 열기
+
+    for (long long i = 0; i < 10; ++i) {
+        sp = 7, fp = 15;
+
+        for (int j = sp; j < fp; ++j) {
+            Binary_Tree[j] = read_input[j - 7][cnt[j - 7]];
+        }
+
+
+        for (long long j = 0; j < 3; ++j) {
+            for (long long pos = sp; pos < fp; pos += 2) {
+                Binary_Tree[(pos - 1) / 2] = MIN(Binary_Tree[pos], Binary_Tree[pos + 1]);
+            }
+            fp = sp - 1;
+            sp /= 2;
+        }
+
+        winner = Binary_Tree[0];
+        left = 1, right = 2;
+
+        for (long long j = 0; j < 3; j++) {
+            if (Binary_Tree[left] == winner) {
+                winner_path[2 - j] = left;
+                right = (left + 1) * 2;
+                left = left * 2 + 1;
+            }
+            else if (Binary_Tree[right] == winner) {
+                winner_path[2 - j] = right;
+                left = right * 2 + 1;
+                right = (right + 1) * 2;
+            }
+        }
+
+        cnt[winner_path[0] - 7]++;
+        fout << winner << '\n';
+
+        for (long long j = 0; j < 4; ++j) {
+            fout << winner_path[j] << ' ';
+        }
+        fout << '\n';
+
+    }
+
+    fout.close();
+
+    for (int i = 0;i < 8;++i) {
+        delete[] read_input[i];
+    }
+
+    delete[] read_input;
 
     return 0;
 }
